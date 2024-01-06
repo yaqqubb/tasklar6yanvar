c6562a5b38a6b8acd1820009fd719e553f41731f
yaqqubb <yagubma@code.edu.az>
2024-01-06 23:17:29 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _6yanvarders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inheritance 
+
+        }
+    }
+    public class Car
+    {
+        string Marka;
+        string Model;
+        string Price;
+        
+        static void Info()
+        {
+            Console.WriteLine(
+        }
+
+    }
+}
